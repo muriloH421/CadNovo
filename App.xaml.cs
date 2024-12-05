1a0c64ea598c137c208327bdb6d3144b501f341a
murilo <basiliomurilo421@gmail.com>
2024-12-05 14:32:09 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CadNovo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
